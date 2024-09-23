cb0846449d41ffe3ef4ebfe41b602c38b54f1c53
evgeny <hatber.index@gmail.com>
2024-09-23 16:05:11 +0300

    add shadcn

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,57 +1,5 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import request from '@/api/axios';
 
-interface ApiResponse<T> {
-  data: T;
-}
-
-const API_URL = process.env.API_URL;
 const API_BASENAME = process.env.API_BASENAME;
-const API_KEY = process.env.API_KEY;
 
-const axiosConfig: AxiosRequestConfig = {
-  baseURL: `${API_URL}`,
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-    'Content-Type': 'application/json'
-  }
-};
-
-const axiosInstance: AxiosInstance = axios.create(axiosConfig);
-
-axiosInstance.interceptors.request.use(
-  async (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(new Error(`Request error: ${error.message}`));
-  }
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(new Error(`Response error: ${error.message}`));
-  }
-);
-
-const fetchApi = async <T>(url: string): Promise<ApiResponse<T>> => {
-  try {
-    const response: AxiosResponse<T> = await axiosInstance.get(url);
-    return { data: response.data };
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.message);
-    } else {
-      throw new Error('Unexpected error');
-    }
-  }
-};
-
-const api = {
-  getData: () =>
-    fetchApi<any>(`${API_BASENAME}todos/1`)
-};
-
-export default api;
+export const getTodos = () => request.get<any>(`${API_BASENAME}todos/1`);
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './assets/global.css';
+import '@/assets/global.css';
 
-import App from './App';
+import AppViewer from '@/AppViewer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppViewer />
   </React.StrictMode>
 );
